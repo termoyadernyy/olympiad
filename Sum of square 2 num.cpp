f85e192865f22ba72d4f92fe8a5e9aca3faaae35
tape <90852088+termoyadernyy@users.noreply.github.com>
2024-12-04 17:16:27 +0200

    Create Sum of square 2 num.cpp

---
+++ Sum of square 2 num.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long a, b;
+    
+    // Введення двох цілих чисел
+    cin >> a >> b;
+    
+    // Обчислення суми квадратів
+    long long result = a * a + b * b;
+    
+    // Виведення результату
+    cout << result << endl;
+
+    return 0;
+}
