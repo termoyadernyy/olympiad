c020554b2a7af89934ca20e8e372b1e4cb8d9352
tape <90852088+termoyadernyy@users.noreply.github.com>
2024-12-04 17:20:14 +0200

    Update Summer.cpp

--- Summer.cpp
+++ Summer.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 int main() {
-    int a, b, c;
-    cin >> a >> b >> c;  // Зчитуємо довжини сторін трикутника
+    int month;
+    cin >> month;  // Зчитуємо номер місяця
 
-    // Перевіряємо тип трикутника
-    if (a == b && b == c) {
-        cout << 1 << endl;  // Рівносторонній трикутник
-    } else if (a == b || b == c || a == c) {
-        cout << 2 << endl;  // Рівнобедрений трикутник
-    } else {
-        cout << 3 << endl;  // Різносторонній трикутник
+    // Використовуємо складені умови для визначення пори року
+    if (month == 3 || month == 4 || month == 5) {
+        cout << "Spring" << endl;  // Весна
+    } else if (month == 6 || month == 7 || month == 8) {
+        cout << "Summer" << endl;  // Літо
+    } else if (month == 9 || month == 10 || month == 11) {
+        cout << "Autumn" << endl;  // Осінь
+    } else if (month == 12 || month == 1 || month == 2) {
+        cout << "Winter" << endl;  // Зима
     }
 
     return 0;
 }
+
